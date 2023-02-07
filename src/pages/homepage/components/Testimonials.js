200597ecba133e851747da6d2923999785bb774b
Corey Stewart <decadentsavant@gmail.com>
2023-02-07 06:36:57 -0500

    Style updates

--- src/pages/homepage/components/Testimonials.js
+++ src/pages/homepage/components/Testimonials.js
@@ -1,11 +1,12 @@
 import testimonialsData from "../data/TestimonialsData";
 import TestimonialsCard from "./TestimonialsCard";
+import "../../homepage/components/Testimonials.css";
 
 const Testimonials = () => {
     return (
         <section>
-            <h2>Testimonials</h2> 
-            <div className="even-columns">
+            <h2 className="testimonials-title subtitle">Testimonials</h2> 
+            <div className="testimonials-cards even-columns">
                 <TestimonialsCard {...testimonialsData.testimonial1} />
                 <TestimonialsCard {...testimonialsData.testimonial2} />
                 <TestimonialsCard {...testimonialsData.testimonial3} />
--- src/pages/homepage/components/TestimonialsCard.js
+++ src/pages/homepage/components/TestimonialsCard.js
@@ -2,17 +2,19 @@ import star from "../../../images/star.svg";
 
 const TestimonialsCard = (props) => {
     return (
-        <div>
-            <div className="even-columns">
+        <div className="testimonials-card">
+            <div className="testimonials-stars">
                 {Array.from({ length: 5 }, (_, i) => (
                     <img src={star} alt="star" />
                 ))}
             </div>
-            <div>
+            <div className="image-name">
                 <img
+                className="testimonials-image"
                     src={require("../../../images/" + props.image)}
                     alt={props.name}
                 />
+                <div className="testimonials-spacer"></div>
                 <p> {props.name}</p>
             </div>
             <div>{props.quote}</div>
