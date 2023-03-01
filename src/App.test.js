ac8bd6854177bb2d85fa215dc64a79d416d38442
Corey Stewart <decadentsavant@gmail.com>
2023-03-01 18:55:41 -0500

    can't figure out test

--- src/App.test.js
+++ src/App.test.js
@@ -6,13 +6,16 @@ test("sanity check", () => {
     expect(true).toBe(true);
 });
 
-// not sure why this fails don't understand error message
+// not sure why this fails
+// BookingForm won't render. Not sure why. Please help.
+
 test("renders the BookingForm", () => {
     render(<BookingForm />);
     const formElement = screen.getByTestId("form");
     expect(formElement).toBeInTheDocument();
 });
 
+// same...
 test("renders the form button", () => {
     render(<BookingForm />);
     const button = screen.getByTestId("button");
